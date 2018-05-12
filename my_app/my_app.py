071f8f17fba7855b6f74645d7c2c4bdf73e1762c
michalgol <m1k3m4n@gmail.com>
2018-05-12 09:17:45 +0000

    add sth

--- my_app/my_app.py
+++ my_app/my_app.py
@@ -1,5 +1,5 @@
 from flask import Flask
-
+from flask import render_template
 app = Flask(__name__)
 
 @app.route("/")
@@ -8,3 +8,18 @@ def hello():
 
 if __name__ == '__main__':
   app.run(host="0.0.0.0", port=8080, debug=T)
+
+@app.route("/order-animation")
+def order_animation():
+  return render_template(
+    "order.html",
+    invitation = "Welcome at ani creation"
+  )
+
+@app.route('/upload')
+def upload():
+  if 'my_file' not in request.files:
+    return "sth wrong"
+  upl_file = request.files['my_file']
+  storage = new S#Storage(os.get, s3)
+  storage.save(path="")
