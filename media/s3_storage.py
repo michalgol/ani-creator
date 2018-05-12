b6f55c79038c7caf5df5206116792973132d0257
michalgol <m1k3m4n@gmail.com>
2018-05-12 08:59:54 +0000

    add flask

--- media/s3_storage.py
+++ media/s3_storage.py
@@ -1,7 +1,18 @@
+import botocore
+
 class S3MediaStorage:
   
-  def save(self, path, file_to_be_uploaded):
-    pass
+  def __init__(self, s3, git_name):
+    self.s3 = s3
+    self.git_name = git_name
+
+  def save(self, path, to_be_uploaded):
+    bucket = self.s3.Bucket(self.git_name)
+    bucket.put_object(Key=path, Body=to_be_uploaded)
   
   def contains(self, path):
-    return True
+    try:
+      s3.Object(self.git_name, path).load()
+      return True
+    except botocore.exceptions.ClientError as e:
+      return False
--- tests/test_s3_media_storage.py
+++ tests/test_s3_media_storage.py
@@ -1,22 +1,25 @@
 import unittest
+import os, boto3
 from media.s3_storage import S3MediaStorage 
 class TestS3Storage(unittest.TestCase):
 
   def test_it_allow_store_files(self):
     #Arrange
     storage = self.there_is_s3_storage()
-    file_to_be_up = self.there_is_file()
+    to_be_up = self.there_is_file()
     #Act
     storage.save(
       path="my/test/path.txt",
-      file_to_be_uploaded=file_to_be_up 
+      to_be_uploaded = to_be_up 
     )
-    #Asert
+    #Assert
     assert False == storage.contains(path='not-ets')
     assert storage.contains(path='my/test/path.txt')
   
   def there_is_s3_storage(self):
-    return S3MediaStorage()
+    git_name = os.getenv('APP_BUCKET_NAME')
+    s3 = boto3.resource('s3')
+    return S3MediaStorage(s3, git_name)
   
   def there_is_file(self):
     pass
